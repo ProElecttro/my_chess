c4280c41879cc772b7c933faf9c44fa53a1847cf
Purushottam <131853935+ProElecttro@users.noreply.github.com>
2024-04-18 18:12:18 +0530

    minor changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import crypto from 'crypto'
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import crypto from 'crypto'
 
-import styles from './style.module.css'
+// import styles from './style.module.css'
 
-const Homepage = () => {
-    const [roomCode, setRoomCode] = useState('')
+// const Homepage = () => {
+//     const [roomCode, setRoomCode] = useState('')
 
-    const generateRoomCode = () => {
-        const randomHex = crypto.randomBytes(4).toString('hex');
-        setRoomCode(randomHex);
-    }
+//     const generateRoomCode = () => {
+//         // const randomHex = crypto.randomBytes(4).toString('hex');
+//         setRoomCode("randomHex");
+//     }
 
-    return (
-        <div className={styles.homepage}>
-            <div className={styles.container}>
-                <div className={styles.join}>
-                    <input 
-                        type="text"
-                        placeholder='Enter Game ID'
-                        value={roomCode}
-                        onChange={(e) => setRoomCode(e.target.value)}
-                    />
-                    <Link to='/game'><button onClick={joinRoom}> JOIN </button></Link>
-                </div>
-                <p id={styles.or}>OR</p>
-                <button onClick={generateRoomCode}>Create a Game Room</button>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className={styles.homepage}>
+//             <div className={styles.container}>
+//                 <div className={styles.join}>
+//                     <input 
+//                         type="text"
+//                         placeholder='Enter Game ID'
+//                         value={roomCode}
+//                         onChange={(e) => setRoomCode(e.target.value)}
+//                     />
+//                     <Link to='/game'><button onClick={joinRoom}> JOIN </button></Link>
+//                 </div>
+//                 <p id={styles.or}>OR</p>
+//                 <button onClick={generateRoomCode}>Create a Game Room</button>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Homepage
+// export default Homepage
